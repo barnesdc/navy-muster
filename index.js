3726d8846da8bb7728877721ed55dfe5e6c757f1
dantebarnes <dcbarnes@aggies.ncat.edu>
2020-09-15 16:31:18 -0400

    another job

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const fs = require("fs");
 const sms = require("./send_sms");
 app = express();
 
-cron.schedule("00 08 * * *", function () {
+cron.schedule("32 16 * * *", function () {
   sms.sendMessage();
   console.log("Sending check-in message");
 });
 
-cron.schedule("00 13 * * *", function () {
+cron.schedule("33 16 * * *", function () {
   sms.sendMessage();
   console.log("Sending check-in message");
 });
