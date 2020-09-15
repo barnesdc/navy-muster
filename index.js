078fe96294a4cef4711da749e55b7b858eee534a
dantebarnes <dcbarnes@aggies.ncat.edu>
2020-09-15 16:28:16 -0400

    update cron jobs

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const fs = require("fs");
 const sms = require("./send_sms");
 app = express();
 
-cron.schedule("27 16 * * *", function () {
+cron.schedule("00 08 * * *", function () {
+  sms.sendMessage();
+  console.log("Sending check-in message");
+});
+
+cron.schedule("00 13 * * *", function () {
   sms.sendMessage();
   console.log("Sending check-in message");
 });
